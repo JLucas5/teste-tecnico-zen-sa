3d62c39686bb17d39e14642701df36497aac4b27
João Lucas Pacheco A. Bastos <joaolucaspachecoab@gmail.com>
2020-12-06 11:01:26 -0300

    Backend - MongoDB Atlas Connected

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const routes = require('./routes')
 
 const app = express()
+
+mongoose.connect('mongodb+srv://zensadb:zensadb@cluster0.f1kph.mongodb.net/zensadb?retryWrites=true&w=majority', {
+     useNewUrlParser: true,
+     useUnifiedTopology: true
+})
 
 app.use(express.json())
 
