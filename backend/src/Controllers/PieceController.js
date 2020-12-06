a8097dcc913bd464ab21a27c4824424fbf9fff80
João Lucas Pacheco A. Bastos <joaolucaspachecoab@gmail.com>
2020-12-06 11:35:21 -0300

    Backend - Controller Methods Created

---
+++ backend/src/Controllers/PieceController.js
@@ -0,0 +1,29 @@
+const Piece = require("../Models/Piece")
+
+module.exports = {
+    async store(req, res){
+
+        const {name, applicationVehicle = "", liquidWeight, grossWeight} = req.body
+
+        const piece = await Piece.create({
+            name, applicationVehicle, liquidWeight, grossWeight
+        })
+
+        return res.json(piece)
+    },
+
+    async show(req, res) {
+        const pieceList = await Piece.find()
+
+        return res.json(pieceList)
+    },
+    
+    async delete(req, res) {
+		const { piece_id } = req.body
+
+		await Piece.findByIdAndDelete(piece_id)
+
+		return res.json({ warning: 'Piece deleted' })
+    },
+    
+}
